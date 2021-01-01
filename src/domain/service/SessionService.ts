e60d80bbc108ca19948045d3e7083febb1246cbd
Maximilian Beck <contact@maximilianbeck.de>
2021-01-01 17:27:52 +0100

    #8: Implement domain logic to add a topic

--- src/domain/service/SessionService.ts
+++ src/domain/service/SessionService.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Session } from '../model/Session';
 import { AbstractSessionPersistenceService } from '../persistence/AbstractSessionPersistenceService';
+import { Topic } from '../model/Topic';
 
 @Injectable()
 export class SessionService {
@@ -8,5 +9,15 @@ export class SessionService {
 
     public async create(session: Session): Promise<Session> {
         return this.sessionPersistenceService.create(session);
+    }
+
+    public async addTopic(sessionId: string, topic: Topic): Promise<Topic> {
+        const session = await this.sessionPersistenceService.findById(sessionId);
+
+        session.addTopic(topic);
+
+        const savedSession = await this.sessionPersistenceService.save(session);
+
+        return savedSession.getTopics().find((savedTopic) => savedTopic.getExternalId() === topic.getExternalId());
     }
 }
